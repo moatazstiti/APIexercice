0dfa0afa47a2e10ef23cb84088bbb9652493f469
moatazstiti <moatazstt@gmail.com>
2024-12-19 20:06:48 +0100

    adding api exercice

---
+++ src/UserCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const UserCard = ({user}) => {
+  return (
+    <div>
+        <li>
+            <h3>name:{user.name}</h3>
+            <p>username:{user.username}</p>
+            <p>email{user.email}</p>
+            <p></p>
+            <p></p>
+            <p></p>
+
+
+
+
+
+
+        </li>
+      
+    </div>
+  )
+}
+
+export default UserCard
